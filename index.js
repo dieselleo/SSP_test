0b3f84847777eb2bbf1cc0c50b4f2c721299d7cc
dieselleo <diesel.leonardo@gmail.com>
2021-11-22 16:42:47 +0000

    Application ready

--- index.js
+++ index.js
@@ -4,4 +4,29 @@ const   http = require('http'),
         fs = require('fs'),
         xmlParse = require('xslt-processor').xmlParse,
         xsltProcess = require('xslt-processor').xsltProcess,
-        xml2js = require('xml2js')
+        xml2js = require('xml2js');
+
+const   router = express(),
+        server = http.createServer(router);
+
+router.get('/',function(req, res){
+    
+    res.writeHead(200,{'Content-Type' : 'text/html'});
+
+    let xml = fs.readFileSync('test.xml', 'utf8'),
+        xsl = fs.readFileSync('test.xsl', 'utf8');
+
+    let doc = xmlParse(xml),
+        stylesheet = xmlParse(xsl);
+    
+    let result = xsltProcess(doc, stylesheet);
+
+    res.end(result.toString());
+
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function()
+{
+    const addr = server.address();
+    console.log("Server listening at ", addr.address + ":" +addr.port)
+});
